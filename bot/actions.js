6c9c030863e574366a4ac569ded781aaeec7e7f1
Valentin Barakov <valentin.barakov@gmail.com>
2018-05-12 22:37:26 +0300

    no message

--- bot/actions.js
+++ bot/actions.js
@@ -22,11 +22,12 @@ module.exports = {
               })
             }
             let msgs =[]
-            msgs.push(new TextMessage(`Hi, ${userProfile.name}! Nice to meet you.`,keyboards.teams))
+            msgs.push(new TextMessage(`Hi, ${userProfile.name}! Nice to meet you.`))
             msgs.push(new TextMessage(`Pick team that you support`,keyboards.teams))
-           // botSendMsg(bot,msgs,userProfile.id,0)
+            console.log(userProfile.id)
+           botSendMsg(bot,msgs,userProfile.id,0)
            //onFinish(new TextMessage(`${userProfile.name} Pick team that you support`,keyboards.teams));
-            onFinish(new TextMessage(`${userProfile.name} Pick team that you support`,keyboards));
+           // onFinish(new TextMessage(`${userProfile.name} Pick team that you support`,keyboards));
           })
        /* if (context === 'winner') {
           console.log(context)
