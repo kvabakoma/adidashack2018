3efb9a652628ec8de05e130fca676a1b075b9963
Valentin Barakov <valentin.barakov@gmail.com>
2018-05-13 02:18:41 +0300

    no message

--- server/controllers/botRedirectCotroller.js
+++ server/controllers/botRedirectCotroller.js
@@ -8,11 +8,21 @@ let source = "" +
   " <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>" +
   "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>" +
   "<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>" +
-  "<style> body{background-color:rgb(67,40,124);padding-top:5%} </style>" +
+  "<style> body{" +
+  "background-color:rgb(67,40,124);" +
+  "padding-top:5%} " +
+  ".startBtn{" +
+  "border-radius: 0;background-color: black;color: white" +
+  "}" +
+  ".startBtn:hover{" +
+  "background-color: white;color: black" +
+  "}" +
+  "</style>" +
   "<div class='col-md-4 col-md-offset-4'>" +
   "<h1 style='text-align: center '>{{invitedBy}} те покани:</h1>" +
   "<div class='col-sm-10 col-sm-offset-1 '> <img style='width:100%;margin: 0 auto' src='http://res.cloudinary.com/dqyb8sdlc/image/upload/v1526159753/avatar_mykjj7.png' alt=''></div>" +
-  "<div style=' display: table;width: 20%;bottom: 8%'><a href='{{deeplink}}'><button class='btn btn-primary btn-lg'>Участвай </button></a></div>" +
+  "<div style=' display: table;width: 20%;bottom: 8%'><a href='{{deeplink}}'>" +
+  "<button class='startBtn col-md-4 col-lg-offset-4' style='' class='btn btn-primary btn-lg'>Участвай </button></a></div>" +
   "</div>";
 let template = Handlebars.compile(source);
 
