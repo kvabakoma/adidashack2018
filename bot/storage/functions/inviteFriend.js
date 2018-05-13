163b65ea095fa7b89e29efc6870688e1c2681194
Valentin Barakov <valentin.barakov@gmail.com>
2018-05-13 04:04:06 +0300

    no message

--- bot/storage/functions/inviteFriend.js
+++ bot/storage/functions/inviteFriend.js
@@ -24,7 +24,7 @@ module.exports = ( message, response) => {
    msg.push(new TextMessage('Виж офертата на Пулс - може да спечелиш: '+siteUrl))
 */
  //userController.updateStep(viberId,'waiting')
-  let msg = [UrlMessage('https://demo.kvaba.com/adidas18')]
+  let msg = [new UrlMessage('https://demo.kvaba.com/adidas18')]
   msg.push(new TextMessage('За да поканиш приятели, задръж две секунди  върху долното съобщение и натсни "Forward"'))
    msg.push( new TextMessage(siteUrl))
   actions.sendMessages(msg,0,response)
