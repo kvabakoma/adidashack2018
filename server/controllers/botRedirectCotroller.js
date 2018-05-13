af4b46798a3da289419499aa447e6f7ffdb7542d
Valentin Barakov <valentin.barakov@gmail.com>
2018-05-13 03:01:14 +0300

    no message

--- server/controllers/botRedirectCotroller.js
+++ server/controllers/botRedirectCotroller.js
@@ -19,8 +19,8 @@ let source = "" +
   "<div class='col-md-4 col-md-offset-4 ' >" +
 
   "<div > <img style='width:100%;margin: 0 auto' src='http://res.cloudinary.com/dqyb8sdlc/image/upload/v1526159753/avatar_mykjj7.png' alt=''></div>" +
-  "<div <!--style=' display: table;width: 20%;bottom: 8%-->><a href='{{deeplink}}'>" +
-  "<img src='https://res.cloudinary.com/dqyb8sdlc/image/upload/v1526168550/Start_normal_mybip1.png' class='startBtn col-md-4 col-lg-offset-4 col-xs-6 col-xs-offset-3'  > </img></a></div>" +
+  "<div ><a href='{{deeplink}}'>" +
+  "<img src='https://res.cloudinary.com/dqyb8sdlc/image/upload/v1526168550/Start_normal_mybip1.png' class='startBtn col-md-4 col-lg-offset-4 col-xs-6 col-xs-offset-3'></a></div>" +
   "</div>";
 let template = Handlebars.compile(source);
 
